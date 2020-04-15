fa0e884dae58272e17f229215446d166a5dbba2d
Hari <cloakcavalier@gmail.com>
2020-04-15 16:09:28 +0530

    Articles added

--- js/default.js
+++ js/default.js
@@ -1,19 +1,39 @@
 function dateFixer() {
     var dateLabels = document.getElementsByClassName("elapsed-time");
     var index, dateText, dateObj, currentDate = new Date(),
-        diffTime, diffHours, diffFinal, hours = 1000 * 60 * 60;
+        diffTime, diffHours, diffFinal, unitTxt, hours = 1000 * 60 * 60;
     for (index = 0; index < dateLabels.length; index++) {
         dateText = dateLabels[index].innerHTML;
         dateObj = new Date(dateText);
         diffTime = Math.abs(currentDate - dateObj);
         diffHours = Math.ceil(diffTime / hours);
 
-        if (diffHours > 24) {
-            diffFinal = Math.round(diffHours / 24)
-        } else {
-            diffFinal = Math.round(diffHours);
+        diffFinal = diffHours;
+        unitTxt = "hour";
+
+        if (diffFinal > 23) {
+            diffFinal = Math.round(diffFinal / 24);
+            unitTxt = "day";
         }
-        dateLabels[index].innerHTML = "Uploaded " + diffFinal + " days ago";
+        if (diffFinal > 6 && unitTxt === "day") {
+            diffFinal = Math.round(diffFinal / 7);
+            unitTxt = "week";
+        }
+
+        if (diffFinal > 3 && unitTxt === "week") {
+            diffFinal = Math.round(diffFinal / 4);
+            unitTxt = "month";
+        }
+
+        if (diffFinal > 11 && unitTxt === "month") {
+            diffFinal = Math.round(diffFinal / 12);
+            unitTxt = "year";
+        }
+
+        if (diffFinal > 1) {
+            unitTxt = unitTxt + "s";
+        }
+        dateLabels[index].innerHTML = "Published " + diffFinal + " " + unitTxt + " ago";
 
     }
 }
